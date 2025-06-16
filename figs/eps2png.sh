6ba0a3161b88dc57f7bb59177ce2e74a9aa8d44d
Russell Goyder <russellgoyder@users.noreply.github.com>
2025-06-15 22:35:36 -0700

    Nuke

--- figs/eps2png.sh
+++
@@ -1,5 +0,0 @@
-
-for x in MainArena SurfaceFrame DialFrame Gnomon ShadowTriangle EquationOfTime HorizontalDialZoom1 ArbitraryDial ArcticDial
-do
-    inkscape -b FFFFFF --export-filename=$x.png $x.eps
-done
--- figs/eps2svg.sh
+++
@@ -1,6 +0,0 @@
-
-# note not DialFrame.svg because I saved it as svg after fixing a typo using Inkscape
-for x in SurfaceFrame Gnomon ShadowTriangle EquationOfTime HorizontalDialZoom1 ArbitraryDial ArcticDial
-do
-    inkscape --export-filename=$x.svg $x.eps
-done
